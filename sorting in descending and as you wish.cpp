3542ef510d455629f13464d2c1250cf22e179851
Nyctophiliac1918 <64465581+Nyctophiliac1918@users.noreply.github.com>
2020-07-01 01:08:55 +0530

    Create sorting in descending and as you wish.cpp

---
+++ sorting in descending and as you wish.cpp
@@ -0,0 +1,56 @@
+
+// C++ program to demonstrate descending order sort using 
+// greater<>(). 
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int main() 
+{ 
+    int arr[] = {1, 5, 8, 9, 6, 7, 3, 4, 2, 0}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+  
+    sort(arr, arr+n, greater<int>()); 
+  
+    cout << "Array after sorting : \n"; 
+    for (int i = 0; i < n; ++i) 
+        cout << arr[i] << " "; 
+  
+    return 0; 
+} 
+
+
+
+
+// A C++ program to demonstrate STL sort() using 
+// our own comparator 
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+// An interval has a start time and end time 
+struct Interval 
+{ 
+    int start, end; 
+}; 
+  
+// Compares two intervals according to staring times. 
+bool compareInterval(Interval i1, Interval i2) 
+{ 
+    return (i1.start < i2.start); 
+} 
+  
+int main() 
+{ 
+    Interval arr[] =  { {6,8}, {1,9}, {2,4}, {4,7} }; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+  
+    // sort the intervals in increasing order of 
+    // start time 
+    sort(arr, arr+n, compareInterval); 
+  
+    cout << "Intervals sorted by start time : \n"; 
+    for (int i=0; i<n; i++) 
+       cout << "[" << arr[i].start << "," << arr[i].end 
+            << "] "; 
+  
+    return 0; 
+} 
